9569f3036d88baeca313dd4debc6aea6f49f1179
Jon Peck <jpeck@fluxsauce.com>
2013-07-24 12:35:47 -0700

    Added cron checks, removed vendor specific options from commands that don't require them.

---
+++ Report/Cron.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * Contains \SiteAudit\Report\Cron.
+ */
+
+class SiteAuditReportCron extends SiteAuditReportAbstract {
+  /**
+   * Implements \SiteAudit\Report\Abstract\getLabel().
+   */
+  public function getLabel() {
+    return dt('Cron');
+  }
+
+  /**
+   * Implements \SiteAudit\Report\Abstract\getCheckNames().
+   */
+  public function getCheckNames() {
+    return array(
+      'enabled',
+      'running',
+      'last',
+    );
+  }
+}
