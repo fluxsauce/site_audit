f854f4b4b0d53d033c2549df7807624a9a4c3007
Shivanshu Agrawal <shivanshuag@gmail.com>
2015-05-18 12:24:50 +0530

    added ManagedFileCount check

---
+++ Report/Codebase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Contains \SiteAudit\Report\Codebase.
+ */
+
+class SiteAuditReportCodebase extends SiteAuditReportAbstract {
+  /**
+   * Implements \SiteAudit\Report\Abstract\getLabel().
+   */
+  public function getLabel() {
+    return dt('Codebase');
+  }
+
+}
