5e8e4181972a547b2276adc173628c7ccbae6947
Shivanshu Agrawal <shivanshuag@gmail.com>
2015-05-19 23:21:20 +0530

    Added ContentType check

---
+++ Report/Content.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Contains \SiteAudit\Report\Content.
+ */
+
+class SiteAuditReportContent extends SiteAuditReportAbstract {
+  /**
+   * Implements \SiteAudit\Report\Abstract\getLabel().
+   */
+  public function getLabel() {
+    return dt('Content');
+  }
+
+}
