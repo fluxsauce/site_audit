a6b0ef953d41f06fe7d1d557385eb6813a9acb67
Shivanshu Agrawal <shivanshuag@gmail.com>
2015-06-24 12:26:57 +0530

    Added Status Report

---
+++ Report/Status.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains \SiteAudit\Report\Status.
+ */
+
+/**
+ * Class SiteAuditReportStatus.
+ */
+class SiteAuditReportStatus extends SiteAuditReportAbstract {
+  /**
+   * Implements \SiteAudit\Report\Abstract\getLabel().
+   */
+  public function getLabel() {
+    return dt('Status');
+  }
+
+}
