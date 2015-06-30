2bcb52a2c82867fcd615bcb79a59b6d8c64ffb43
Shivanshu Agrawal <shivanshuag@gmail.com>
2015-06-30 11:07:02 +0530

    Added front end report

---
+++ Report/FrontEnd.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file
+ * Contains \SiteAudit\Report\Insights.
+ */
+
+/**
+ * Class SiteAuditReportFrontEnd.
+ */
+class SiteAuditReportFrontEnd extends SiteAuditReportAbstract {
+  /**
+   * Override parent constructor to provide argument support.
+   *
+   * @param string $url
+   *   URL of site to test.
+   * @param string $key
+   *   Google API key.
+   */
+  public function __construct($url, $key) {
+    $this->registry['url'] = $url;
+    $this->registry['key'] = $key;
+    parent::__construct();
+  }
+
+  /**
+   * Implements \SiteAudit\Report\Abstract\getLabel().
+   */
+  public function getLabel() {
+    return dt('Front End');
+  }
+
+}
