0aad710288edd448f947b655726d726d74d77691
Shivanshu Agrawal <shivanshuag@gmail.com>
2015-07-16 16:34:08 +0530

    Edit bash script to handle drush installed through composer

--- run-tests-drush.sh
+++ run-tests-drush.sh
@@ -8,11 +8,20 @@
 # Any parameters that may be passed to `phpunit` may also be used
 # with this script.
 
+export UNISH_NO_TIMEOUTS=y
+export UNISH_DRUPAL_MAJOR_VERSION=8
 DRUSH_PATH="`which drush`"
 DRUSH_DIRNAME="`dirname -- "$DRUSH_PATH"`"
+
+# If the path is inside composer
+if [[ $DRUSH_DIRNAME == *".composer/vendor/"* ]]
+then
+    DRUSH_DIRNAME="`dirname -- "$DRUSH_DIRNAME"`"
+    DRUSH_DIRNAME+="/drush/drush"
+fi
+
 # The following line is needed is you use a `drush` that differs from `which drush`
 # export UNISH_DRUSH=$DRUSH_PATH
-
 if [ $# = 0 ] ; then
    phpunit --configuration="$DRUSH_DIRNAME/tests" tests
 else
