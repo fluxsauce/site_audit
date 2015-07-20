d99cdeb540945ae36a53f25eb52f7c067afa1ee3
Shivanshu Agrawal <shivanshuag@gmail.com>
2015-07-20 13:40:49 +0530

    Added tests for duplicate titles and unused content types

--- tests/contentReportTest.php
+++ tests/contentReportTest.php
@@ -60,4 +60,141 @@ class ContentReportCase extends SiteAuditTestAbstract {
     $this->assertEquals(\SiteAuditCheckAbstract::AUDIT_CHECK_SCORE_PASS, $output->checks->SiteAuditCheckContentVocabulariesUnused->score);
   }
 
+  /**
+   * Warn if a content entity type is unused.
+   */
+  public function testContentContentEntityTypesUnusedWarn() {
+    $this->drush('pm-enable', array('taxonomy'), $this->options);
+    $this->drush('audit-content', array(), $this->options + array(
+        'detail' => NULL,
+        'json' => NULL,
+      ));
+    $output = $this->getOutput();
+    print $output;
+    $output = json_decode($output);
+    $this->assertEquals(\SiteAuditCheckAbstract::AUDIT_CHECK_SCORE_WARN, $output->checks->SiteAuditCheckContentContentEntityTypesUnused->score);
+  }
+
+  /**
+   * Pass if no unused content entity type.
+   */
+  public function testContentContentEntityTypesUnusedPass() {
+    $this->drush('pm-enable', array('node'), $this->options);
+    $this->drush('pm-enable', array('taxonomy'), $this->options);
+    // Create a node of content type content.
+    $eval1 = "\$nodeStore = \\Drupal::entityManager()->getStorage('node');";
+    $eval1 .= "\$node = \$nodeStore->create(array(";
+    $eval1 .= "'nid' => NULL,";
+    $eval1 .= "'type' => 'node',";
+    $eval1 .= "'title' => 'Site Audit',";
+    $eval1 .= "'uid' => 1,";
+    $eval1 .= "'revision' => 0,";
+    $eval1 .= "'status' => TRUE,";
+    $eval1 .= "'promote' => 0,";
+    $eval1 .= "'created' => 1,";
+    $eval1 .= "'langcode' => 'und',";
+    $eval1 .= "));";
+    $eval1 .= "\$node->save();";
+
+    // Create a taxonomy term of vocabulary taxonomy_term.
+    $eval2 = "\$termStore = \\Drupal::entityManager()->getStorage('taxonomy_term');";
+    $eval2 .= "\$term = \$termStore->create(array(";
+    $eval2 .= "'name' => 'siteaudit',";
+    $eval2 .= "'description' => 'siteaudit rocks',";
+    $eval2 .= "'format' => filter_fallback_format(),";
+    $eval2 .= "'weight' => 5,";
+    $eval2 .= "'langcode' => 'und',";
+    $eval2 .= "'vid' => 'taxonomy_term',";
+    $eval2 .= "'parent' => array(0),";
+    $eval2 .= "));";
+    $eval2 .= "\$term->save();";
+    $this->drush('php-eval', array($eval1), $this->options);
+    $this->drush('php-eval', array($eval2), $this->options);
+    $this->drush('audit-content', array(), $this->options + array(
+        'detail' => NULL,
+        'json' => NULL,
+      ));
+    $output = json_decode($this->getOutput());
+    $this->assertEquals(\SiteAuditCheckAbstract::AUDIT_CHECK_SCORE_PASS, $output->checks->SiteAuditCheckContentContentEntityTypesUnused->score);
+  }
+
+  /**
+   * Pass if no nodes with duplicate titles.
+   */
+  public function testContentDuplicateTitlesPass() {
+    $this->drush('pm-enable', array('node'), $this->options);
+    $eval1 = "\$nodeStore = \\Drupal::entityManager()->getStorage('node');";
+    $eval1 .= "\$node = \$nodeStore->create(array(";
+    $eval1 .= "'nid' => NULL,";
+    $eval1 .= "'type' => 'node',";
+    $eval1 .= "'title' => 'Site Audit',";
+    $eval1 .= "'uid' => 1,";
+    $eval1 .= "'revision' => 0,";
+    $eval1 .= "'status' => TRUE,";
+    $eval1 .= "'promote' => 0,";
+    $eval1 .= "'created' => 1,";
+    $eval1 .= "'langcode' => 'und',";
+    $eval1 .= "));";
+    $eval1 .= "\$node->save();";
+
+    $eval1 .= "\$node = \$nodeStore->create(array(";
+    $eval1 .= "'nid' => NULL,";
+    $eval1 .= "'type' => 'node',";
+    $eval1 .= "'title' => 'Site Audit 1',";
+    $eval1 .= "'uid' => 1,";
+    $eval1 .= "'revision' => 0,";
+    $eval1 .= "'status' => TRUE,";
+    $eval1 .= "'promote' => 0,";
+    $eval1 .= "'created' => 1,";
+    $eval1 .= "'langcode' => 'und',";
+    $eval1 .= "));";
+    $eval1 .= "\$node->save();";
+    $this->drush('php-eval', array($eval1), $this->options);
+    $this->drush('audit-content', array(), $this->options + array(
+        'detail' => NULL,
+        'json' => NULL,
+      ));
+    $output = json_decode($this->getOutput());
+    $this->assertEquals(\SiteAuditCheckAbstract::AUDIT_CHECK_SCORE_PASS, $output->checks->SiteAuditCheckContentDuplicateTitles->score);
+  }
+  /**
+   * Warn if nodes with duplicate titles present.
+   */
+  public function testContentDuplicateTitlesWarn() {
+    $this->drush('pm-enable', array('node'), $this->options);
+    $eval1 = "\$nodeStore = \\Drupal::entityManager()->getStorage('node');";
+    $eval1 .= "\$node = \$nodeStore->create(array(";
+    $eval1 .= "'nid' => NULL,";
+    $eval1 .= "'type' => 'node',";
+    $eval1 .= "'title' => 'Site Audit',";
+    $eval1 .= "'uid' => 1,";
+    $eval1 .= "'revision' => 0,";
+    $eval1 .= "'status' => TRUE,";
+    $eval1 .= "'promote' => 0,";
+    $eval1 .= "'created' => 1,";
+    $eval1 .= "'langcode' => 'und',";
+    $eval1 .= "));";
+    $eval1 .= "\$node->save();";
+
+    $eval1 .= "\$node = \$nodeStore->create(array(";
+    $eval1 .= "'nid' => NULL,";
+    $eval1 .= "'type' => 'node',";
+    $eval1 .= "'title' => 'Site Audit',";
+    $eval1 .= "'uid' => 1,";
+    $eval1 .= "'revision' => 0,";
+    $eval1 .= "'status' => TRUE,";
+    $eval1 .= "'promote' => 0,";
+    $eval1 .= "'created' => 1,";
+    $eval1 .= "'langcode' => 'und',";
+    $eval1 .= "));";
+    $eval1 .= "\$node->save();";
+    $this->drush('php-eval', array($eval1), $this->options);
+    $this->drush('audit-content', array(), $this->options + array(
+        'detail' => NULL,
+        'json' => NULL,
+      ));
+    $output = json_decode($this->getOutput());
+    $this->assertEquals(\SiteAuditCheckAbstract::AUDIT_CHECK_SCORE_WARN, $output->checks->SiteAuditCheckContentDuplicateTitles->score);
+  }
+
 }
