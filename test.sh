fe93557bbcae4f4e0b44f7685654aac3b4bdef7a
Jon Peck <jpeck@fluxsauce.com>
2015-07-27 09:33:17 -0700

    Cleanup.

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 # This script is a modified form of the script present in devel
-# module for running drush commnadfile tests. The original can be found at
+# module for running drush command file tests.
+# 
+# The original can be found at
 # http://cgit.drupalcode.org/devel/tree/run-tests-drush.sh?id=refs/heads;id2=8.x-1.x
+# 
 # This script will run phpunit-based test classes using Drush's
 # test framework.  First, the Drush executable is located, and
 # then phpunit is invoked, pointing to Drush's phpunit.xml as
@@ -16,7 +19,7 @@ export UNISH_DRUPAL_MAJOR_VERSION=8
 DRUSH_PATH="`which drush`"
 DRUSH_DIRNAME="`dirname -- "$DRUSH_PATH"`"
 
-# If the path is inside composer
+# Path is inside Composer.
 if [[ $DRUSH_DIRNAME == *".composer/vendor/"* ]]
 then
     DRUSH_DIRNAME="`dirname -- "$DRUSH_DIRNAME"`"
