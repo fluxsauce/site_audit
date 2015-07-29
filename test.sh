b301fd392af15d2f9306889748f15f046d74ee84
Shivanshu Agrawal <shivanshuag@gmail.com>
2015-07-29 23:55:54 +0530

    include drush as dev dependency

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 # This script is a modified form of the script present in devel
 # module for running drush command file tests.
-# 
+#
 # The original can be found at
 # http://cgit.drupalcode.org/devel/tree/run-tests-drush.sh?id=refs/heads;id2=8.x-1.x
-# 
+#
 # This script will run phpunit-based test classes using Drush's
 # test framework.  First, the Drush executable is located, and
 # then phpunit is invoked, pointing to Drush's phpunit.xml as
@@ -16,19 +16,11 @@
 
 export UNISH_NO_TIMEOUTS=y
 export UNISH_DRUPAL_MAJOR_VERSION=8
-DRUSH_PATH="`which drush`"
-DRUSH_DIRNAME="`dirname -- "$DRUSH_PATH"`"
-
-# Path is inside Composer.
-if [[ $DRUSH_DIRNAME == *".composer/vendor/"* ]]
-then
-    DRUSH_DIRNAME="`dirname -- "$DRUSH_DIRNAME"`"
-    DRUSH_DIRNAME+="/drush/drush"
-fi
 
 # The following line is needed is you use a `drush` that differs from `which drush`
-# export UNISH_DRUSH=$DRUSH_PATH
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DRUSH_DIRNAME=$DIR/vendor/drush/drush
+export UNISH_DRUSH=$DRUSH_DIRNAME/drush
 if [ $# = 0 ] ; then
    $DIR/vendor/bin/phpunit --configuration="$DRUSH_DIRNAME/tests" tests
 else
