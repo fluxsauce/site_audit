01cb858710f9639e8a7fa0487a2ecf0cc70386e8
jrglasgow <jrglasgow@36590.no-reply.drupal.org>
2018-07-17 14:56:04 -0600

    Issue #2971356 by jrglasgow: Port Block Report and Checks

---
+++ src/Plugin/SiteAuditCheck/BlockEnabled.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\site_audit\Plugin\SiteAuditCheck\BlockEnabled
+ */
+
+namespace Drupal\site_audit\Plugin\SiteAuditCheck;
+
+use Drupal\site_audit\Plugin\SiteAuditCheckBase;
+
+/**
+ * Provides the BlockEnabled Check.
+ *
+ * @SiteAuditCheck(
+ *  id = "block_enabled",
+ *  name = @Translation("Block status"),
+ *  description = @Translation("Check to see if enabled"),
+ *  report = "block"
+ * )
+ */
+class BlockEnabled extends SiteAuditCheckBase {
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getResultFail() {
+    return $this->t('Block caching is not enabled!');
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getResultInfo() {
+    return $this->t('Block is not enabled.');
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getResultPass() {
+    return $this->t('Block is enabled.');
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getResultWarn() {
+    return $this->t("Block is enabled, but there is no default theme. Consider disabling block if you don't need it.");
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getAction() {}
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function calculateScore() {
+    if (!\Drupal::service('module_handler')->moduleExists('block')) {
+      $this->abort = TRUE;
+      return SiteAuditCheckBase::AUDIT_CHECK_SCORE_INFO;
+    }
+    $this->registry->theme_default = \Drupal::config('system.theme')->get('default');
+    if (!$this->registry->theme_default) {
+      return SiteAuditCheckBase::AUDIT_CHECK_SCORE_WARN;
+    }
+    return SiteAuditCheckBase::AUDIT_CHECK_SCORE_PASS;
+  }
+
+}
---
+++ src/Plugin/SiteAuditReport/Block.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\site_audit\Plugin\SiteAuditReport\Block
+ */
+
+namespace Drupal\site_audit\Plugin\SiteAuditReport;
+
+use Drupal\site_audit\Plugin\SiteAuditReportBase;
+
+/**
+ * Provides a Block Report.
+ *
+ * @SiteAuditReport(
+ *  id = "block",
+ *  name = @Translation("Block"),
+ *  description = @Translation("Drupal's Blocks")
+ * )
+ */
+class Block extends SiteAuditReportBase {}
