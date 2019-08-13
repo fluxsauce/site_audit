1544d70cc314a3b1f52071cf241e83c35183b780
thiagogomesverissimo <thiagogomesverissimo@1024062.no-reply.drupal.org>
2019-08-13 08:33:37 -0600

    Issue #2971358 by thiagogomesverissimo: Port Codebase Report and Checks

---
+++ src/Plugin/SiteAuditCheck/CodebaseSizeAll.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\site_audit\Plugin\SiteAuditCheck\CodebaseSizeAll
+ */
+
+namespace Drupal\site_audit\Plugin\SiteAuditCheck;
+
+use Drupal\site_audit\Plugin\SiteAuditCheckBase;
+
+/**
+ * Provides the CronLast Check.
+ *
+ * @SiteAuditCheck(
+ *  id = "codebase_sizeall",
+ *  name = @Translation("Size of entire site"),
+ *  description = @Translation("Determine the size of the site root; does not include remote mounts."),
+ *  report = "codebase",
+ *  weight = -1,
+ * )
+ */
+class CodebaseSizeAll extends SiteAuditCheckBase {
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getResultFail() {
+    return $this->t('Empty, or unable to determine the size due to a permission error.');
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getResultInfo() {
+
+    if ($this->registry->size_all_kb < 1024) {
+      return $this->t('Total size: @size_all_kbkB', array(
+        '@size_all_kb' => number_format($this->registry->size_all_kb),
+      ));
+    }
+    return $this->t('Total size: @size_all_mbMB', array(
+      '@size_all_mb' => number_format($this->registry->size_all_kb / 1024, 2),
+    ));
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getResultPass() {}
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getResultWarn() {}
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getAction() {}
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function calculateScore() {
+    try {
+      exec('du -s -k -x ' . DRUPAL_ROOT, $result);
+      $this->registry->size_all_kb = trim($result[0]);
+      if (!$this->registry->size_all_kb) {
+        $this->abort = TRUE;
+        return SiteAuditCheckBase::AUDIT_CHECK_SCORE_FAIL;
+      }
+      return SiteAuditCheckBase::AUDIT_CHECK_SCORE_INFO;
+    }
+    catch (Exception $e) {
+      return SiteAuditCheckBase::AUDIT_CHECK_SCORE_FAIL;
+    }
+  }
+
+}
---
+++ src/Plugin/SiteAuditReport/Codebase.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\site_audit\Plugin\SiteAuditReport\BestPractices
+ */
+
+namespace Drupal\site_audit\Plugin\SiteAuditReport;
+
+use Drupal\site_audit\Plugin\SiteAuditReportBase;
+
+/**
+ * Provides a Database Report.
+ *
+ * @SiteAuditReport(
+ *  id = "codebase",
+ *  name = @Translation("Codebase"),
+ *  description = @Translation("Drupal Codebase Best Practices and Settings")
+ * )
+ */
+class Codebase extends SiteAuditReportBase {}
